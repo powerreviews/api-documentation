5fa0993fb16361318ab7247f47a340da3d4c7b0c
Eva Connors <eva.m.connors@gmail.com>
2017-09-19 14:14:38 -0500

    re-hopefully fixed path to aws

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ cp ../api-specs/* .
 
 sed -i 's/..\/api-specs/\/\/developers.powerreviews.com/g' index.html
 
-/usr/local/bin/aws s3 sync . s3://developers.powerreviews.com --storage-class REDUCED_REDUNDANCY --acl public-read --delete
+/usr/bin/aws s3 sync . s3://developers.powerreviews.com --storage-class REDUCED_REDUNDANCY --acl public-read --delete
 
 # CLI cloudfront features are in preview - for now, TTL on cloudfront is set to 600 seconds.
 # aws cloudfront create-invaldiation --distribution-id E2VEPY1WRUNO4
