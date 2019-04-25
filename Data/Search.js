10bd034b3a1fd06a1bfad8d46bb4cd562f2ab3c9
DESKTOP-UPG8D6E\Temp-Tech-Writer <techwriter@powerreviews.com>
2019-04-25 14:45:02 -0500

    Updating Scroll

--- Data/Search.js
+++ Data/Search.js
@@ -1,1 +1,1 @@
-define({r:0.5,st:'Stem',n:1,t:[0],u:['../Content/Contact Us.htm'],s:['0'],p:[['0',8],['going',5],['upc',1]]});
+define({r:0.5,st:'Stem',n:1,t:[0],u:['../Content/Contact Us.htm'],s:['0'],p:[['0',8],['getallreviews',11],['total',16]]});
--- Resources/Scripts/require.config.js
+++ Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=636917218023814192'
+    urlArgs: 't=636918000287457000'
 });
